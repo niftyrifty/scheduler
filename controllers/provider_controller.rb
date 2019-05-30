93cfc477cc6746c590d2d7f414ec49204d6219a8
rifayat <rifayat@umich.edu>
2019-05-30 16:52:50 -0400

    WIP: Provider has work to be done.

--- controllers/provider_controller.rb
+++ controllers/provider_controller.rb
@@ -1,16 +1,30 @@
 require 'tty-prompt'
 require '../models/provider'
 
-prompt = TTY::Prompt.new
 
-name = prompt.ask('Name:')
-phone_number = prompt.ask('Phone number:')
+
+#$name = prompt.ask('Name:')
+#$phone_number = prompt.ask('Phone number:')
+
+# @providers = 
 
 class Provider_Controller
-  provider = Provider.new(name, phone_number)
-  puts provider.name
-  puts provider.phone_number
+  # provider = Provider.new($name, $phone_number)
+  # puts provider.getName()
+  # puts provider.getPhoneNumber()
+
+  # Need help figuring out how to get create/add working 
+  def self.create 
+    prompt = TTY::Prompt.new 
+    name = prompt.ask('Name:')
+    phone_number = prompt.ask('Phone number:')
+    $providers[name] = Provider.new(name, phone_number)
+    puts("Provider Name: "+ $providers[name].getName)
+    puts("Provider Phone #: " + $providers[name].getPhoneNumber) 
+  end
 end
+
+Provider_Controller.create
 
 # class Provider_Controller
 #   def create
