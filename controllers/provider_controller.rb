0ebdf51df2b63e03313d81820d8ae39f88c1a172
Pearl Zeng <zhuzeng@umich.edu>
2019-05-31 01:07:54 -0400

    Refactor provider controller and service controller
    1. Fix alignments
    2. Have both a variable to keep all instances
    and a def all that can be called outside of the class
    3. Add additional content to print to screen to help users understand
    the context

--- controllers/provider_controller.rb
+++ controllers/provider_controller.rb
@@ -5,34 +5,63 @@ require '../models/service'
 class Provider_Controller
   attr_accessor :providers
 
-  @service_list = ['Mind Reading', 'Demonic Exorcism', 'Potion Therapy', 'Liver Transplants']
+  @service_types = ['Mind Reading', 
+                    'Demonic Exorcism', 
+                    'Potion Therapy', 
+                    'Liver Transplants']
+
+  @providers = [Provider.new('Junius', '234-486-9800', @service_types),     
+                Provider.new('Pearl', '978-123-5768', @service_types),
+                Provider.new('Rifty', '008-111-2590', @service_types)]
 
   def self.all
-    @providers = [Provider.new('Junius', '234-486-9800', @service_list), Provider.new('Pearl', '978-123-5768', @service_list)]
     @providers
   end
 
   def self.index
-    @providers.each do |provider|
-      puts "#{provider.name}'s phone number is #{provider.phone_number}. (S)he provides these services: #{provider.services}"
+    puts "Here's the current list of providers:"
+
+    @providers.map do |provider|
+      puts "#{provider.name}'s phone number is #{provider.phone_number}."
+      puts "(S)he provides these services: #{provider.services} every day of the week except for #{provider.day_off}."
+      puts "––––––––––"
     end
   end
 
   def self.add
     prompt = TTY::Prompt.new 
-    name = prompt.ask('Name:')
+
+    name = prompt.ask('Provider Name:')
     phone_number = prompt.ask('Phone number:')
-    choices = @service_list
-    services = prompt.multi_select("Please choose three services from the following list:", choices)
-    provider = Provider.new(name, phone_number, services)
+    choices = @service_types
+    services = prompt.multi_select("Please choose services from the 
+                following list:", choices)
+    day_off = prompt.select('Day off:', ['Monday', 'Tuesday', 'Wednesday', 
+                'Thursday', 'Friday', 'Saturday', 'Sunday'])
+
+    provider = Provider.new(name, phone_number, services, day_off)
     @providers << provider
+
+    puts "\n"
     puts "#{provider.name} is successfully added."
+    puts "\n"
+    puts self.index
   end
 
   def self.remove
     prompt = TTY::Prompt.new
     options = @providers.map { |provider| provider.name}
     choice = prompt.select("Pick a provider to remove", options)
+
     @providers = @providers.reject { |provider| provider.name == choice}
+    
+    puts "\n"
+    puts "#{choice} is successfully removed."
+    puts "\n"
+    puts self.index
   end
+
+  # def self.view_calendar
+  #   TODO
+  # end
 end
--- controllers/service_controller.rb
+++ controllers/service_controller.rb
@@ -1,14 +1,23 @@
 require 'tty-prompt'
-require './models/service'
+require '../models/service'
 
 class Service_Controller
   attr_accessor :services
 
-  @services = [Service.new('Hair cut', 20, 1), Service.new('Massage', 50, 2)]
+  @services = [Service.new('Hair cut', 20, 1), 
+              Service.new('Massage', 50, 2),
+              Service.new('Makeup', 100, 2)]
+
+  def self.all
+    @services
+  end
 
   def self.index
-    @services.each do |service|
+    puts "Here's the current list of services:"
+    
+    @services.map do |service|
       puts "#{service.name} costs $#{service.price}, and takes about #{service.duration} hours."
+      puts "––––––––––"
     end
   end
 
@@ -17,15 +26,26 @@ class Service_Controller
     name = prompt.ask('Name:')
     price = prompt.ask('Price($):')
     duration = prompt.ask('Duration(hours):')
+    
     service = Service.new(name, price, duration)
     @services << service
+
+    puts "\n"
     puts "#{service.name} is successfully added."
+    puts "\n"
+    puts self.index
   end
 
   def self.remove
     prompt = TTY::Prompt.new
     options = @services.map { |service| service.name}
     choice = prompt.select("Pick a service to delete", options)
-    @services = @services.reject { |service| service.name == choice }
+    
+    @services = @services.delete_if { |service| service.name == choice }
+
+    puts "\n"
+    puts "#{choice} is successfully removed."
+    puts "\n"
+    puts self.index
   end
 end
