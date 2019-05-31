9541037ef22ca5a87ad780d1a0af6ab8afc24ee5
Pearl Zeng <zhuzeng@umich.edu>
2019-05-31 09:08:49 -0400

    Add list of scheduled appointments to Provider model

--- models/provider.rb
+++ models/provider.rb
@@ -1,11 +1,12 @@
 class Provider
-  attr_accessor :name, :phone_number, :services, :day_off
+  attr_accessor :name, :phone_number, :services, :day_off, :scheduled_appointments
 
-  def initialize(name, phone_number, services=[], day_off='Sunday')
+  def initialize(name, phone_number, services=[], day_off='Sunday', scheduled_appointments=[])
       @name = name
       @phone_number = phone_number
       @services = services
       @day_off = day_off
+      @scheduled_appointments = scheduled_appointments
   end
 end
  
