5f2da509671ce1d712c51fe936f89d62e78770e2
Pearl Zeng <zhuzeng@umich.edu>
2019-05-31 01:04:35 -0400

    Add day off param to Provider

--- models/provider.rb
+++ models/provider.rb
@@ -1,10 +1,11 @@
 class Provider
-  attr_accessor :name, :phone_number, :services
+  attr_accessor :name, :phone_number, :services, :day_off
 
-  def initialize(name, phone_number, services=[])
+  def initialize(name, phone_number, services=[], day_off='Sunday')
       @name = name
       @phone_number = phone_number
       @services = services
+      @day_off = day_off
   end
 end
  
