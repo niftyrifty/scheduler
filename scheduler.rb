fd4ffa9b10d113bf1cc6c972c905dde0b967b111
Pearl Zeng <zhuzeng@umich.edu>
2019-05-31 12:29:44 -0400

    Refactor: use look up table and .send to DRY code

--- scheduler.rb
+++ scheduler.rb
@@ -4,44 +4,44 @@ require './controllers/provider_controller'
 require './controllers/appointment_controller'
 
 class Scheduler
-	OPTIONS = ["Add Provider", "Remove Provider", "Add Service", "Remove Service", "Schedule an Appointment", "View Schedule of a particular Provider",  "End Session"]
-
-	#change this into a hash of hashes
 	FUNCTION_LOOKUP = {
+		"Add Provider" => {
+			controller: ProviderController,
+			method_name: :add
+		},
+		"Remove Provider" => {
+			controller: ProviderController,
+			method_name: :remove
+		},
 		"Add Service" => {
 			controller: ServiceController,
 			method_name: :add
 		},
-		"Add Provider" => :add,
-		"Remove Provider" => :remove
+		"Remove Service" => {
+			controller: ServiceController,
+			method_name: :remove
+		},
+		"Schedule an Appointment" => {
+			controller: AppointmentController,
+			method_name: :add
+		},
+		"View Schedule of a Particular Provider" => {
+			controller: ProviderController,
+			method_name: :view_schedule
+		}
 	}
 	
 	continue_program = true
 	prompt = TTY::Prompt.new
-
 	while continue_program do
-		choice = prompt.select("What would you like to do?", OPTIONS)
-		
-		# Refactor gradually
-		# 1. make every line identical to line 34-35 first
-		# 2. and then collapse it down
-		case choice
-		when "Add Provider"	
-			ProviderController.send(FUNCTION_LOOKUP[choice])
-		when "Remove Provider"
-			ProviderController.send(choice)
-		when "Add Service"
-			# ServiceController.add
-			FUNCTION_LOOKUP[choice][:controller].send(FUNCTION_LOOKUP[choice][:method_name])
-		when "Remove Service"
-			ServiceController.remove
-		when "Schedule an Appointment"
-			AppointmentController.add
-		when "View Schedule of a particular Provider"
-			ProviderController.view_schedule
-		when "End Session"
-			continue_program = false
-		end
+		choice = prompt.select("What would you like to do?", FUNCTION_LOOKUP.keys)
+		FUNCTION_LOOKUP[choice][:controller].send(FUNCTION_LOOKUP[choice][:method_name])
+	
+		# TODO: rewrite the end session function
+		# Could consider using a key down, reference tty docs
+		# when "End Session"
+		# 	continue_program = false
+		# end
 	end
 end
     
